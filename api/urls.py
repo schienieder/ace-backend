104356c700337a8a3e261963bbfe4b9c227e0779
Justine Rhei Torres <schieniedertorres@gmail.com>
2021-07-07 02:08:01 +0800

    Added a profile route and getprofileview

--- api/urls.py
+++ api/urls.py
@@ -3,12 +3,18 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from api.views import CreateAccountView, GetAccountView, AllBusinessPartnersView
+from api.views import (
+    CreateAccountView,
+    GetAccountView,
+    GetProfileView,
+    AllBusinessPartnersView,
+)
 
 urlpatterns = [
     path("register/", CreateAccountView.as_view(), name="registerAccount"),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("account/<int:pk>", GetAccountView.as_view(), name="accountView"),
+    path("profile/<int:pk>", GetProfileView.as_view(), name="profileView"),
     path("partners/", AllBusinessPartnersView.as_view(), name="allBusinessPartners"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,11 +1,16 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework import views
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.exceptions import AuthenticationFailed
-from api.serializers import AccountSerializer, BusinessPartnerSerializer
-from api.models import Account, BusinessPartner
+from api.serializers import (
+    AccountSerializer,
+    ProfileSerializer,
+    BusinessPartnerSerializer,
+)
+from api.models import Account, Profile, BusinessPartner
 
 # Create your views here.
 class CreateAccountView(generics.CreateAPIView):
@@ -28,6 +33,25 @@ class GetAccountView(generics.RetrieveAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = AccountSerializer
     queryset = Account.objects.all()
+
+
+class GetProfileView(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
+
+    def get_queryset(self):
+        return self.queryset
+
+    def get_object(self):
+        queryset = self.get_queryset()
+        queryset = Profile.objects.filter(account__id=self.request.user.id).first()
+        return queryset
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
 
 
 class AllBusinessPartnersView(generics.ListAPIView):
