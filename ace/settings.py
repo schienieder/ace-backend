ff7276ca236ee7001b1c3c9dddf18348c7c06822
schieneider <schieniedertorres@gmail.com>
2021-08-30 23:00:16 +0800

    Allowed all in allowed_hosts

--- ace/settings.py
+++ ace/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://alas-creatives.vercel.app/"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
